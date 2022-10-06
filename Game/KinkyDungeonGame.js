a7a041c4de8c328b15f9b2540f30870162febecd
Ada <gubertpol0@gmail.com>
2022-10-05 22:27:04 -0700

    Enemy counts

--- Game/KinkyDungeonGame.js
+++ Game/KinkyDungeonGame.js
@@ -813,7 +813,7 @@ function KinkyDungeonPlaceEnemies(spawnPoints, InJail, Tags, BonusTags, Floor, w
 		{requiredTags: ["boss"], tags: [], currentCount: 0, maxCount: 0.001},
 		{requiredTags: ["miniboss"], tags: [], currentCount: 0, maxCount: 0.1},
 		{requiredTags: ["elite"], tags: [], currentCount: 0, maxCount: 0.2},
-		{requiredTags: ["minor"], tags: [], currentCount: 0, maxCount: 0.25},
+		{requiredTags: ["minor"], tags: [], currentCount: 0, maxCount: 0.2},
 		{requiredTags: [KinkyDungeonFactionTag[randomFaction]], tags: [KinkyDungeonFactionTag[randomFaction]], currentCount: 0, maxCount: 0.1},
 	];
 	if (KDGameData.MapMod) {
