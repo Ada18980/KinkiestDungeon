b18ff49ad5f4322a326cc22b20536d67dc62077f
Ada <gubertpol0@gmail.com>
2022-12-29 22:57:58 -0800

    Reintroduce rope tentacles

--- Game/KinkyDungeonEnemiesList.js
+++ Game/KinkyDungeonEnemiesList.js
@@ -1226,7 +1226,7 @@ let KinkyDungeonEnemies = [
 
 
 	{name: "RopeKraken", faction: "KinkyConstruct", clusterWith: "construct", tags: KDMapInit(["construct", "poisonresist", "soulimmune", "melee", "boss", "elite", "unflinching", "fireweakness", "slashweakness", "chainresist", "hunter"]),
-		ignorechance: 0.75, followRange: 1, AI: "hunt",
+		ignorechance: 0.75, followRange: 1, AI: "hunt",  summon: [{enemy: "RopeMinion", range: 2.5, count: 4, strict: true}],
 		spells: ["RopeEngulf", "SummonRopeTentacle"], spellCooldownMult: 1, spellCooldownMod: 0, ignoreflag: ["kraken"], disarm: 0.25,
 		visionRadius: 10, maxhp: 60, minLevel: 3, weight:-31, movePoints: 4, attackPoints: 2, attack: "Spell", attackWidth: 1, attackRange: 1, power: 6, dmgType: "grope",
 		terrainTags: {"secondhalf":16, "lastthird":5, "boss": -80, "open": 20, "passage": -60, "ropeAnger": 20, "ropeRage": 60, "increasingWeight":0.5}, allFloors: true, shrines: ["Rope"],
