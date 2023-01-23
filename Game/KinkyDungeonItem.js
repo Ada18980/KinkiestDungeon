247f99c8ac78dcf6956334ca9c581793e5227e09
Ada <gubertpol0@gmail.com>
2023-01-22 20:30:52 -0800

    Fix softlock

--- Game/KinkyDungeonItem.js
+++ Game/KinkyDungeonItem.js
@@ -230,6 +230,9 @@ function KinkyDungeonDrawHeart() {
 	DrawTextKD(TextGet("StatMana").replace("CURRENT/MAX", "" + KinkyDungeonStatManaMax * KDMaxStatStart), 1250 + 250/2, 650, "#ffffff", KDTextGray2);
 	DrawTextKD(TextGet("StatWill").replace("CURRENT/MAX", "" + KinkyDungeonStatWillMax * KDMaxStatStart), 1550 + 250/2, 650, "#ffffff", KDTextGray2);
 
+	// Fix softlock
+	if (!(KinkyDungeonStatDistractionMax < KDMaxStat && KinkyDungeonStatStaminaMax < KDMaxStat && KinkyDungeonStatManaMax < KDMaxStat && KinkyDungeonStatWillMax < KDMaxStat)) KinkyDungeonDrawState = "Game";
+
 	DrawButtonVis(650, 700, 250, 60, TextGet("KinkyDungeonHeartDistraction"), KinkyDungeonStatDistractionMax < KDMaxStat ? "#ffffff" : "#999999");
 	DrawButtonVis(950, 700, 250, 60, TextGet("KinkyDungeonHeartStamina"), KinkyDungeonStatStaminaMax < KDMaxStat ? "#ffffff" : "#999999");
 	DrawButtonVis(1250, 700, 250, 60, TextGet("KinkyDungeonHeartMana"), KinkyDungeonStatManaMax < KDMaxStat ? "#ffffff" : "#999999");
