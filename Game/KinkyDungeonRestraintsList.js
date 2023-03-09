caec0b9fddb95e42b3d0e83d4bd740a57bef0b82
Ada <gubertpol0@gmail.com>
2023-03-09 12:50:11 -0800

    ..

--- Game/KinkyDungeonRestraintsList.js
+++ Game/KinkyDungeonRestraintsList.js
@@ -410,10 +410,10 @@ const KinkyDungeonRestraints = [
 		Modules: [1, 1, 1, 1],
 		factionColor: [[0], [1], [3]],
 		Color: ["#222222", "#b927a8", "#000000", "#499ed6", "#222222", "#000000"],
-		Group: "ItemArms", bindarms: true, bindhands: 1.0, power: 12, weight: 0, strictness: 0.2,
+		Group: "ItemArms", bindarms: true, bindhands: 1.0, power: 15, weight: 0, strictness: 0.2,
 		escapeChance: {"Struggle": -0.2, "Cut": -.3, "Remove": -0.3, "Pick": -0.1},
 		limitChance: {"Struggle": -0.2, "Cut": 0.3, "Remove": 0.1, "Unlock": 0.75}, // Hard to escape the arms box by struggling
-		maxwill: 0.1,
+		maxwill: 0.1, DefaultLock: "Red",
 		enemyTags: {"cyberdollheavy": 1},
 		playerTags: {}, minLevel: 0, allFloors: true, shrine: ["Latex", "Straitjackets", "Cyber"]},
 
