b3cd064134917e294d86060fbfa7a8d111ab09d3
Ada <gubertpol0@gmail.com>
2023-04-11 23:52:46 -0700

    Fix shop desc

--- Game/KinkyDungeonShrine.js
+++ Game/KinkyDungeonShrine.js
@@ -354,7 +354,7 @@ function KinkyDungeonDrawShrine() {
 			i += 1;
 			for (let N = 0; N < textSplit2.length; N++) {
 				DrawTextFitKD(textSplit2[N],
-					KDModalArea_x+650, KDModalArea_y + 120 - shopHeight * 50 + i * descSpacing, 380 * (textSplit2[N].length / 40), "#ffffff", undefined, 20,);
+					KDModalArea_x+650, KDModalArea_y + 120 - shopHeight + i * descSpacing, 380 * (textSplit2[N].length / 40), "#ffffff", undefined, 20,);
 				i++;
 			}
 			// Next button
