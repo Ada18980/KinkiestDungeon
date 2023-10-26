b4a802bb8372480136b4de52a25ca8d2c434477f
Ada18980 <gubertpol0@gmail.com>
2023-10-25 23:49:47 -0700

    hot fix

--- Game/KinkyDungeonRestraints.js
+++ Game/KinkyDungeonRestraints.js
@@ -3044,7 +3044,7 @@ function KDCanAddRestraint(restraint, Bypass, Lock, NoStack, r, Deep, noOverpowe
 		if (!pass) return false;
 	}
 	if (!r) r = KinkyDungeonGetRestraintItem(restraint.Group);
-	let power = powerBonus + (KinkyDungeonRestraintPower(r, false, restraint) * (useAugmentedPower ? KDRestraintPowerMult(KinkyDungeonPlayerEntity, KDRestraint(r), augmentedInventory) : 1));
+	let power = powerBonus + (KinkyDungeonRestraintPower(r, false, restraint) * (r && useAugmentedPower ? KDRestraintPowerMult(KinkyDungeonPlayerEntity, KDRestraint(r), augmentedInventory) : 1));
 	let linkUnder = KDGetLinkUnder(r, restraint, Bypass, NoStack, Deep, securityEnemy);
 
 	let linkableCurrent = r && KDRestraint(r) && KinkyDungeonLinkableAndStricter(KDRestraint(r), restraint, r);
