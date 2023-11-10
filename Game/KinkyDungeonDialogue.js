0678f4a145023a29699bbeaa2a6a8ad809203399
Ada18980 <gubertpol0@gmail.com>
2023-11-10 09:16:51 -0800

    Dialogue

--- Game/KinkyDungeonDialogue.js
+++ Game/KinkyDungeonDialogue.js
@@ -381,7 +381,7 @@ function KDStartDialogInput(Dialogue, Speaker, Click, Personality, enemy) {
 
 function KDDialogueGagged() {
 	let dialogue = KDGetDialogue();
-	let threshold = dialogue.gagThreshold ? dialogue.gagThreshold : 0.01;
+	let threshold = dialogue.gagThreshold ? dialogue.gagThreshold : (KinkyDungeonStatsChoice.get("SmoothTalker") ? 0.99 : 0.01);
 	if (KinkyDungeonGagTotal() >= threshold) return true;
 	return false;
 }
