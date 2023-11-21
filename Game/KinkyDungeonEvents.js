904d76b1e9cbe712095daa52682b1af2e759b28d
YuSaki丶Kanade <53483352+Nep-Timeline@users.noreply.github.com>
2023-11-21 18:20:25 +0800

    Update KinkyDungeonEvents.js

--- Game/KinkyDungeonEvents.js
+++ Game/KinkyDungeonEvents.js
@@ -1492,7 +1492,7 @@ let KDEventMapInventory = {
 				KinkyDungeonStatBlind = Math.max(KinkyDungeonStatBlind + 1, 2);
 
 				for (let A = 0; A < KinkyDungeonPlayer.Appearance.length; A++) {
-					if (KinkyDungeonPlayer.Appearance[A].Asset.Model.Group === "Eyes" || KinkyDungeonPlayer.Appearance[A].Asset.Model.Group === "Eyes2") {
+					if (KinkyDungeonPlayer.Appearance[A].Model.Group === "Eyes" || KinkyDungeonPlayer.Appearance[A].Model.Group === "Eyes2") {
 						let property = KinkyDungeonPlayer.Appearance[A].Property;
 						if (!property || property.Expression !== "Surprised") {
 							KinkyDungeonPlayer.Appearance[A].Property = {Expression: "Surprised"};
