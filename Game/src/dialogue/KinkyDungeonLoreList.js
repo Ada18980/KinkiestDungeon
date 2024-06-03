9fee9836436af78230f16fc6b7f88e6109c2dc21
Ada <gubertpol0@gmail.com>
2024-06-03 02:04:05 -0700

    More spicy lore

--- Game/src/dialogue/KinkyDungeonLoreList.js
+++ Game/src/dialogue/KinkyDungeonLoreList.js
@@ -31,6 +31,22 @@ KDNewLore(["Default", 'grv', 'cat'], "CathCat1", "Willows 17",
 	"I made it to a deeper floor today, and was greeted by a skeleton trying to tie me up. Great.|I've looked into the local name for this mountain, 'Ejahl,' and found a footnote in the history books talking about some petty kingdom that laid siege to Agartha. There's no record of them actually winning the siege, though. I've heard there's a massive library somewhere down here, maybe I'll find answers there.",
 	undefined, undefined, ['Default', 'grv'],
 );
+KDNewLore(["Default", 'cat'], "CathCat2", "Willows 19",
+	"Catherine Willows' Journal (pg. 19)",
+	"I ran into an embarrassed looking blue-haired mage. I caught a glimpse of her undergarments under her cloak. She used a powerful charged attack against me, but I was able to use their homing properties to my advantage and hit her with her own spell.||...was she really wearing no clothes under there?",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'cat'], "CathCat2", "Willows 20",
+	"Catherine Willows' Journal (pg. 20)",
+	"There was a warrior in the catacombs today. She appears to have mastered the art of witty comebacks, as I truly could not come up with anything to say that did not further humiliate myself. Fortunately her insults came at her companions' expense, and they allowed me to defeat her in a solitary duel.",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'cat'], "CathCat3", "Willows 21",
+	"Catherine Willows' Journal (pg. 21)",
+	"I almost found myself the recipient of a powerful binding arrow enchanted with force runes. Had I been hit, I would have been knocked back against the walls with my arms pinned by glowing conjured cuffs. Fortunately, this projectile hit a skeleton that was attacking me instead, and I was able to retreat.",
+	undefined, undefined, ['Default'],
+);
+
 KDNewLore(["Default", 'grv', 'tmb'], "CathTmb", "Willows 24",
 	"Catherine Willows' Journal (pg. 24)",
 	"In my travels I encountered a curious tomb filled with the scent of incense and herbs. Strange statues lined the halls on stone pedestals, standing as an intimidating reminder of what happens to those who disturb the locals...|I encountered resistance from the locals, who seemed to regard outsiders as 'cursed' and sought to capture me in an effort to give me a 'blessing.' I highly doubt that anyone has been truly blessed by their efforts, well-intentioned or not.",
@@ -45,6 +61,48 @@ KDNewLore(["Default", 'tmb'], "CathTmb3", "Willows 26",
 	"Catherine Willows' Journal (pg. 26)",
 	"There are two 'castes' in the Bast cult. The first are the 'blessed,' and the second are their watchful guardians. The unintuitive bit is that the 'blessed' are the ones wrapped up and sealed away inside statues, sarcophagi, and anything else the guardians can stick a person into. The free ones wandering around grabbing people? They're the ones who weren't 'blessed' apparently.",
 	undefined, undefined, ['Default'],
+);
+
+KDNewLore(["Default", 'cat', 'lib'], "CathLib1", "Willows 30",
+	"Catherine Willows' Journal (pg. 30)",
+	"I've found it! The Archmagus' library! Much to my chagrin, however, the place has been ransacked by witches and a curious group of mages in search of inspiration from ancient fashion choices.",
+	undefined, undefined, ['Default', 'cat'],
+);
+KDNewLore(["Default", 'lib'], "CathLib2", "Willows 31",
+	"Catherine Willows' Journal (pg. 31)",
+	"It appears someone got to the Archmagus' writings before I could. Nonetheless, I will continue my search for Vinlaga's personal study. Perhaps her research materials and drafts might shed some light on my condition.",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'lib'], "CathLib3", "Willows 33",
+	"Catherine Willows' Journal (pg. 36)",
+	"I have learned of an ancient artifact, called the Mistress' Staff. Its history and owner are hotly debated, with some attributing it to the Archmagus herself, while others say it was developed by the magical warlords that followed. Most all sources agree that its signature feature is the ability to reverse the effects of the Great Work on any person.",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'lib'], "CathLib4", "Willows 41",
+	"Catherine Willows' Journal (pg. 41)",
+	"There is a treasure trove of ancient history here that the blasted Dressmakers had no use for. I found a map from the 'Kingdom of Ejahl,' with sketches of many islands including this one. Like most of the warlord states, they failed to make a lasting impact on the architecture of the island, but it appears the island was uninhabited for some time between the fall of Agartha and its resettlement by Ejahl.",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'lib'], "CathLib5", "Willows 42",
+	"Catherine Willows' Journal (pg. 42)",
+	"I must find it.|They are looking for it.|They CANNOT be allowed to have it.",
+	undefined, undefined, ['Default'],
+);
+
+KDNewLore(["Default", 'jng', 'cry'], "CathJng1", "Willows 50",
+	"Catherine Willows' Journal (pg. 50)",
+	"I saw my reflection in a pool of water today. My hair looked much more silver than I remember it. I hope this isn't a side effect of all the magic I'd used to make it to this point.",
+	undefined, undefined, ['Default', 'cry'],
+);
+KDNewLore(["Default", 'jng'], "CathJng2", "Willows 51",
+	"Catherine Willows' Journal (pg. 51)",
+	"I heard the bandits are looking for the Mistress' Staff. I'm not too worried--they wouldn't know what to do with it. It's that damned witch I'm worried about.|If my astute reader has come this far, you should beware the Silver Witch. She is working with forces that cannot be trusted.",
+	undefined, undefined, ['Default'],
+);
+KDNewLore(["Default", 'jng', 'cry'], "SW1", "Silver 1",
+	"Silver Witch's Notes #1",
+	"It seems I am not alone in my search for the Staff. To whom it may concern: the Greak Work is a lie, and it will doom us all. If you care for your soul, you need to break the cycle, and for that we need a key: the Mistress' Staff.",
+	undefined, undefined, ['Default', 'cry'],
 );
 
 
