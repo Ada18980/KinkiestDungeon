f053f3a924147e4b104f5cc8b70508fc7680edac
Ada <gubertpol0@gmail.com>
2025-04-09 23:18:53 -0700

    Merge remote-tracking branch 'Enraa/5.4-HeadpatsResize' into 5.5

--- Game/src/base/KinkyDungeon.ts
+++ Game/src/base/KinkyDungeon.ts
@@ -7387,3 +7387,45 @@ function KDNonContextActions(mobile: boolean, textArea: boolean): boolean {
 	}
 	return false;
 }
+
+// Get the canvas offset with respect to the game window.
+// x and y are pixel offsets from the edge of the window to the edge of the game canvas.
+// width and height are window.innerWidth and window.innerHeight respectively.
+// widthscale and heightscale are float values as a % of the game canvas size. These will currently always be the same. 
+//     -> For example, window width 1500 should return a scale of 0.75 because the base canvas is 2000x1000 at this writing.
+// When referencing something such as hardpoints, use this scale to get actual pixel offsets. 
+// canvaswidth and canvasheight are the current actual width and height of the canvas. 
+// Returns { x, y, width, height, widthscale, heightscale, canvaswidth, canvasheight }
+function KDGetWindowCanvasOffset() {
+	let windowwidth = window.innerWidth;
+	let windowheight = window.innerHeight;
+
+	// Determine the ratio of the width x height and height x width
+	let canvaswidth = KinkyDungeonGetCanvas("MainCanvas").width
+	let canvasheight = KinkyDungeonGetCanvas("MainCanvas").height
+	let canvasratio = canvaswidth / canvasheight
+	let canvasratioinvert = Math.pow(canvasratio, -1)
+
+	let offsetobject = { x: 0, y: 0, width: windowwidth, height: windowheight, widthscale: 1.0, heightscale: 1.0, canvaswidth: KinkyDungeonCanvas.width, canvasheight: KinkyDungeonCanvas.height }
+
+	// The window has black bars on top and bottom
+	if ((windowwidth / windowheight) < canvasratio) {
+		offsetobject.y = (windowheight - (windowwidth * canvasratioinvert)) / 2
+		let newscale = windowwidth / KinkyDungeonCanvas.width
+		offsetobject.widthscale = newscale;
+		offsetobject.heightscale = newscale;
+		offsetobject.canvaswidth = KinkyDungeonCanvas.width * newscale
+		offsetobject.canvasheight = KinkyDungeonCanvas.height * newscale
+	}
+	// The window has black bars on left and right
+	if ((windowheight / windowwidth) < canvasratioinvert) {
+		offsetobject.x = (windowwidth - (windowheight * canvasratio)) / 2
+		let newscale = windowheight / KinkyDungeonCanvas.height
+		offsetobject.widthscale = newscale;
+		offsetobject.heightscale = newscale;
+		offsetobject.canvaswidth = KinkyDungeonCanvas.width * newscale
+		offsetobject.canvasheight = KinkyDungeonCanvas.height * newscale
+	}
+
+	return offsetobject
+}
