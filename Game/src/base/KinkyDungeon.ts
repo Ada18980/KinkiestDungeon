f5a5ae8414f9b269f6c91e8af39f139d4eedd52b
Ada <gubertpol0@gmail.com>
2026-01-21 22:21:44 -0800

    .

--- Game/src/base/KinkyDungeon.ts
+++ Game/src/base/KinkyDungeon.ts
@@ -95,7 +95,7 @@ let KDEasterEgg = Math.random() < 0.01;
 let KDBigLanguages = ["CN", "KR", "JP"];
 let KDBigLanguages2 = ["Chinese", "Korean", "Japanese"];
 /** Language List */
-let KDLanguages = ["", "CN", "KR", "JP", "RU", "ES"];
+let KDLanguages = ["", "CN", "KR", "JP", "RU", "ES", "PL"];
 
 let KinkyDungeonPlayerNeedsRefresh = false;
 let KinkyDungeonNextRefreshCheck = 0;
