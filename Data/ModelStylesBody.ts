24f908e88a4299cfbc4e9f4c2e99061ed7eb4a95
Ada <gubertpol0@gmail.com>
2026-02-24 06:56:28 -0800

    prog

--- Data/ModelStylesBody.ts
+++ Data/ModelStylesBody.ts
@@ -10,8 +10,6 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 	"Zombie" : [
 		{
 			Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Torso":{"gamma":1,"saturation":0.35000000000000003,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1},"Head":{"gamma":1,"saturation":0.35000000000000003,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1}}
-		},{
-			Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Torso":{"gamma":1,"saturation":0.35000000000000003,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1},"Head":{"gamma":1,"saturation":0.35000000000000003,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1}}
 		}
 	],
 	"Invisible" : [
@@ -65,7 +63,10 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 	],
 
 	"Demon" : [
-		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+		{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":0.08333333333333333,"contrast":3.0833333333333335,"brightness":0.3833333333333333,"red":0.9333333333333333,"green":0.55,"blue":1.8333333333333333,"alpha":1},
+			Torso: {"gamma":1,"saturation":0.08333333333333333,"contrast":3.0833333333333335,"brightness":0.3833333333333333,"red":0.9333333333333333,"green":0.55,"blue":1.8333333333333333,"alpha":1},
+		}},{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":0.08333333333333333,"contrast":3.0833333333333335,"brightness":0.3833333333333333,"red":0.9333333333333333,"green":0.55,"blue":1.8333333333333333,"alpha":1},
 			Torso: {"gamma":1,"saturation":0.08333333333333333,"contrast":3.0833333333333335,"brightness":0.3833333333333333,"red":0.9333333333333333,"green":0.55,"blue":1.8333333333333333,"alpha":1},
 		}},
@@ -81,9 +82,13 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 
 	"Pale" : [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Head":{"gamma":1,"saturation":0.5666666666666667,"contrast":1.3666666666666667,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1},"Torso":{"gamma":1,"saturation":0.5666666666666667,"contrast":1.3666666666666667,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1}}},
+		{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Head":{"gamma":1,"saturation":0.5666666666666667,"contrast":1.3666666666666667,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1},"Torso":{"gamma":1,"saturation":0.5666666666666667,"contrast":1.3666666666666667,"brightness":1,"red":1,"green":1,"blue":1,"alpha":1}}},
 	],
 	"ElementalLight": [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
+			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
 			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
 		}},
@@ -92,10 +97,16 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1.1,"contrast":1,"brightness":0.9166666666666666,"red":1.7000000000000002,"green":1,"blue":0.8,"alpha":0.9833333333333333},
 			Torso: {"gamma":1,"saturation":1.1,"contrast":1,"brightness":0.9166666666666666,"red":1.7000000000000002,"green":1,"blue":0.8,"alpha":0.9833333333333333},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":1.1,"contrast":1,"brightness":0.9166666666666666,"red":1.7000000000000002,"green":1,"blue":0.8,"alpha":0.9833333333333333},
+			Torso: {"gamma":1,"saturation":1.1,"contrast":1,"brightness":0.9166666666666666,"red":1.7000000000000002,"green":1,"blue":0.8,"alpha":0.9833333333333333},
 		}},
 	],
 	"ElementalCorrupted": [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":0.5166666666666666,"contrast":1,"brightness":0.5666666666666667,"red":1.2666666666666668,"green":1,"blue":1.6,"alpha":0.9833333333333333},
+			Torso: {"gamma":1,"saturation":0.5166666666666666,"contrast":1,"brightness":0.5666666666666667,"red":1.2666666666666668,"green":1,"blue":1.6,"alpha":0.9833333333333333},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":0.5166666666666666,"contrast":1,"brightness":0.5666666666666667,"red":1.2666666666666668,"green":1,"blue":1.6,"alpha":0.9833333333333333},
 			Torso: {"gamma":1,"saturation":0.5166666666666666,"contrast":1,"brightness":0.5666666666666667,"red":1.2666666666666668,"green":1,"blue":1.6,"alpha":0.9833333333333333},
 		}},
@@ -104,10 +115,16 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.7333333333333334,"red":1.2,"green":0.9833333333333333,"blue":0.8166666666666667,"alpha":1},
 			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.7333333333333334,"red":1.2,"green":0.9833333333333333,"blue":0.8166666666666667,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.7333333333333334,"red":1.2,"green":0.9833333333333333,"blue":0.8166666666666667,"alpha":1},
+			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":0.7333333333333334,"red":1.2,"green":0.9833333333333333,"blue":0.8166666666666667,"alpha":1},
 		}},
 	],
 	"Dryad": [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":1,"contrast":2.1,"brightness":0.7000000000000001,"red":0.8833333333333333,"green":1.2,"blue":0.8500000000000001,"alpha":1},
+			Torso: {"gamma":1,"saturation":1,"contrast":2.1,"brightness":0.7000000000000001,"red":0.8833333333333333,"green":1.2,"blue":0.8500000000000001,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1,"contrast":2.1,"brightness":0.7000000000000001,"red":0.8833333333333333,"green":1.2,"blue":0.8500000000000001,"alpha":1},
 			Torso: {"gamma":1,"saturation":1,"contrast":2.1,"brightness":0.7000000000000001,"red":0.8833333333333333,"green":1.2,"blue":0.8500000000000001,"alpha":1},
 		}},
@@ -118,6 +135,9 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":0.016666666666666666,"contrast":1,"brightness":0.8666666666666667,"red":1,"green":1,"blue":1.4833333333333334,"alpha":0.9833333333333333},
 			Torso: {"gamma":1,"saturation":0.016666666666666666,"contrast":1,"brightness":0.8666666666666667,"red":1,"green":1,"blue":1.4833333333333334,"alpha":0.9833333333333333},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":0.016666666666666666,"contrast":1,"brightness":0.8666666666666667,"red":1,"green":1,"blue":1.4833333333333334,"alpha":0.9833333333333333},
+			Torso: {"gamma":1,"saturation":0.016666666666666666,"contrast":1,"brightness":0.8666666666666667,"red":1,"green":1,"blue":1.4833333333333334,"alpha":0.9833333333333333},
 		}},
 	],
 
@@ -125,17 +145,20 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
 			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
+			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
 		}},
 	],
 	"ElementalWater": [
-		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+		{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":0,"contrast":1,"brightness":1,"red":1,"green":1.1372549019607843,"blue":1.0784313725490196,"alpha":1},
 			Torso: {"gamma":1,"saturation":0,"contrast":1,"brightness":1,"red":1,"green":1.1372549019607843,"blue":1.0784313725490196,"alpha":1},
 		}},
 	],
 
 	"ElementalIce": [
-		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+		{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
 			Torso: {"gamma":1,"saturation":1,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1.0833333333333335,"alpha":1},
 		}},
@@ -148,11 +171,18 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 			Head:{"gamma":0.44999999999999996,"saturation":1.5,"contrast":0.8333333333333333,"brightness":0.8833333333333333,"red":1.2,"green":1.2,"blue":1.1,"alpha":1},
 			Torso:{"gamma":0.44999999999999996,"saturation":1.5,"contrast":0.8333333333333333,"brightness":0.8833333333333333,"red":1.2,"green":1.2,"blue":1.1,"alpha":1},
 			Nipples: {"gamma":1,"saturation":1.6,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":0.8},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head:{"gamma":0.44999999999999996,"saturation":1.5,"contrast":0.8333333333333333,"brightness":0.8833333333333333,"red":1.2,"green":1.2,"blue":1.1,"alpha":1},
+			Torso:{"gamma":0.44999999999999996,"saturation":1.5,"contrast":0.8333333333333333,"brightness":0.8833333333333333,"red":1.2,"green":1.2,"blue":1.1,"alpha":1},
+			Nipples: {"gamma":1,"saturation":1.6,"contrast":1,"brightness":1,"red":1,"green":1,"blue":1,"alpha":0.8},
 		}},
 	],
 
 	"Mid": [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1,"saturation":2.4333333333333336,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
+			Torso: {"gamma":1,"saturation":2.4333333333333336,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1,"saturation":2.4333333333333336,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
 			Torso: {"gamma":1,"saturation":2.4333333333333336,"contrast":1,"brightness":0.9333333333333333,"red":1,"green":1,"blue":1,"alpha":1},
 		}},
@@ -161,16 +191,23 @@ let KDModelBody: {[_: string]: KinkyDungeonDress} = {
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1.2,"saturation":1.0166666666666666,"contrast":1.1,"brightness":1.4333333333333333,"red":0.6166666666666667,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
 			Torso: {"gamma":1.2,"saturation":1.0166666666666666,"contrast":1.1,"brightness":1.4333333333333333,"red":0.6166666666666667,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
+		}},{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1.2,"saturation":1.0166666666666666,"contrast":1.1,"brightness":1.4333333333333333,"red":0.6166666666666667,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
+			Torso: {"gamma":1.2,"saturation":1.0166666666666666,"contrast":1.1,"brightness":1.4333333333333333,"red":0.6166666666666667,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
 		}},
 	],
 	"MidTan" : [
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
 			Head: {"gamma":1.2,"saturation":1.0666666666666667,"contrast":1.1,"brightness":1.1666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
 			Torso: {"gamma":1.2,"saturation":1.0666666666666667,"contrast":1.1,"brightness":1.1666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
+		}}, {Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {
+			Head: {"gamma":1.2,"saturation":1.0666666666666667,"contrast":1.1,"brightness":1.1666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
+			Torso: {"gamma":1.2,"saturation":1.0666666666666667,"contrast":1.1,"brightness":1.1666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},
 		}
 		},
 	],
 	"DarkTan" : [
+		{Item: "SmoothBody", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Torso":{"gamma":1.2,"saturation":0.7833333333333334,"contrast":1.1,"brightness":0.9666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},"Head":{"gamma":1.2,"saturation":0.7833333333333334,"contrast":1.1,"brightness":0.9666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1}}},
 		{Item: "Body", Group: "Body", Color: KDBaseWhite, Lost: false, Filters: {"Torso":{"gamma":1.2,"saturation":0.7833333333333334,"contrast":1.1,"brightness":0.9666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1},"Head":{"gamma":1.2,"saturation":0.7833333333333334,"contrast":1.1,"brightness":0.9666666666666667,"red":0.7333333333333334,"green":0.5166666666666666,"blue":0.5333333333333333,"alpha":1}}},
 	],
 };
