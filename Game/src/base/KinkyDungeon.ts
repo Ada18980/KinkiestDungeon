85875b93f462e240c23e6c1304c5aa9dac5f48e7
Ada <gubertpol0@gmail.com>
2026-08-30 03:16:39 -0700

    Fix dress mixing bug

--- Game/src/base/KinkyDungeon.ts
+++ Game/src/base/KinkyDungeon.ts
@@ -6098,6 +6098,7 @@ function KinkyDungeonStartNewGame(Load: boolean = false) {
 	KinkyDungeonNewGame = 0;
 	let cp = KinkyDungeonMapIndex.grv;
 	KDUpdateHardMode();
+	if (!Load) KinkyDungeonNewDress = true;
 	KinkyDungeonInitialize(1, Load);
 	MiniGameKinkyDungeonCheckpoint = "grv";
 	KDMapData.Grid = "";
